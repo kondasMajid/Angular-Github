4d8f14937f1c675efd6f29fcffd0b43ae0ac2953
kondasMajid <kondasmajid@gmail.com>
2020-02-13 23:16:21 -0800

    comits

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,43 +10,43 @@ import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 })
 export class HomeComponent implements OnInit {
 
-  input: FormControl;
-  constructor(private apiService: ApiReqService) {
-    this.input = new FormControl();
-    console.log(this.getUser())
-    console.log(this.getRepos())
-    
-   }
-   inputs;
-   inn;
-   store : []
-   Data: any;
-   @Input() user: string;
-  ngOnInit() {}
+    input: FormControl;
+    constructor(private apiService: ApiReqService) {
+      this.input = new FormControl();
+      console.log(this.getUser())
+      console.log(this.getRepos())
+    }
 
-  // making http request to get the data through the api.
-  getUser(){
-      this.input.valueChanges.pipe(debounceTime(500), distinctUntilChanged()).subscribe((keywords: string) => {
-          this.apiService.getUsers(keywords).subscribe(data=>{
-            this.user = this.apiService.username;
-            this.Data = data;
-            this.Data = Array.of(this.Data)
-            // if(Array.isArray(data)){
-            //   this.Data = data;
-            //   console.log('show', this.Data) 
-            // }
-            // this.store.push(this.Data)
-            console.log('show', this.Data)
-          }, 
-          err => console.log(err),
-          () => console.log('users loaded successfully'))
-        });
+    inputs; // user input vaariable
+    inn;
+    store : []
+    Data: any;
+    @Input() user: string;
+    ngOnInit() {}
+
+    // making http request to get the data through the api.
+    getUser(){
+        this.input.valueChanges.pipe(debounceTime(500), distinctUntilChanged()).subscribe((keywords: string) => {
+            this.apiService.getUsers(keywords).subscribe(data=>{
+              this.user = this.apiService.username;
+              this.Data = data;
+              this.Data = Array.of(this.Data)
+              // if(Array.isArray(data)){
+              //   this.Data = data;
+              //   console.log('show', this.Data) 
+              // }
+              // this.store.push(this.Data)
+              console.log('show', this.Data)
+            }, 
+            err => console.log(err),
+            () => console.log('users loaded successfully'))
+          });
+    }
+
+    //get repos
+    getRepos(){
+      this.apiService.getRepos().subscribe((res) => console.log('repos', res))
+      this.user = this.apiService.username;
+      // console.log(res)
   }
-
-  //get repos
-  getRepos(){
-    this.apiService.getRepos().subscribe((res) => console.log('repos', res))
-    this.user = this.apiService.username;
-    // console.log(res)
-}
 } 
